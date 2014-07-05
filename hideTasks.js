8e5e01af2fb512f18bfca5efb9582138b8abaa28
Jeffrey Burt <jeffrey.m.burt@gmail.com>
2014-07-04 21:06:42 -0400

    main files added

---
+++ hideTasks.js
@@ -0,0 +1,9 @@
+function hideTasks(){
+    var elem = document.getElementById('gadgetcell');
+    elem.style.display = "none";
+    var selem = document.getElementById('maincell');
+    selem.style.marginRight = "0px";
+    var elem = document.getElementById('rhstogglecell');
+    elem.style.display = "none";
+}
+hideTasks();
